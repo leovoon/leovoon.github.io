587f1996ce2c21ec04479ef096c903c1b026c4ff
leovoon <voonlihhaur@gmail.com>
2020-09-10 20:05:59 +0800

    Updates

---
+++ precache-manifest.0545fbcc8abbe880dcfd09afec4d5d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab618311f853535a222f68ef91c4724c",
+    "url": "/leovoon.github.io/index.html"
+  },
+  {
+    "revision": "1f1ce4f953958430556c",
+    "url": "/leovoon.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fd783ea25241e3b65a7",
+    "url": "/leovoon.github.io/static/js/2.18107305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leovoon.github.io/static/js/2.18107305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1ce4f953958430556c",
+    "url": "/leovoon.github.io/static/js/main.20f3897b.chunk.js"
+  },
+  {
+    "revision": "5395d5f5dc3dc36319c0",
+    "url": "/leovoon.github.io/static/js/runtime-main.782eac79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leovoon.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
