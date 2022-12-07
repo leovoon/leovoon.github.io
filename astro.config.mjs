d8b69eea5613181ac7c789d3f1796e2222cbfc47
leovoon <voonlihhaur@gmail.com>
2022-12-07 15:35:23 +0800

    update: add images fix: change text color perf: add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config'
+import purgecss from 'astro-purgecss'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://leovoon.github.io',
+  integrations: [
+    purgecss({
+      fontFace: true,
+      keyframes: true,
+      variables: true,
+      rejected: true,
+    }),
+  ],
 })
--- src/data/projectList.js
+++ src/data/projectList.js
@@ -4,77 +4,110 @@ export const projectList = [
     description: 'React ecommerce with redux-toolkit + rtk-query + graphql',
     githubLink: 'https://github.com/leovoon/react-fc-storefront',
     liveLink: 'https://react-fc-storefront.vercel.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/react-storefront.jpg',
+      webp: 'img/webp/react-storefront.webp',
+    },
   },
   {
     name: 'Pendek',
     description: 'Shorten your url',
     githubLink: 'https://github.com/leovoon/link-shortener',
     liveLink: 'https://pendek.ml/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/pendek.jpg',
+      webp: 'img/webp/pendek.webp',
+    },
   },
   {
     name: 'Tatan',
     description: 'Search Tatan GIFs',
     githubLink: 'https://github.com/leovoon/tatan',
     liveLink: 'https://tatan.vercel.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/tatan.jpg',
+      webp: 'img/webp/tatan.webp',
+    },
   },
   {
     name: 'ws.',
     description: 'WhatsApp this number',
     githubLink: 'https://github.com/leovoon/ws',
     liveLink: 'https://wsthis.vercel.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/ws.jpg',
+      webp: 'img/webp/ws.webp',
+    },
   },
   {
     name: 'DASS',
     description: 'Check your Depression Anxiety Stress Scale',
     githubLink: 'https://github.com/leovoon/dass-nuxt',
     liveLink: 'https://dass-nuxt.vercel.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/dass.jpg',
+      webp: 'img/webp/dass.webp',
+    },
   },
   {
     name: 'release',
     description: 'Spit your thought anonymously',
     githubLink: 'https://github.com/leovoon/release',
     liveLink: 'https://znl.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/znl.jpg',
+      webp: 'img/webp/znl.webp',
+    },
   },
   {
     name: 'Vue Budget',
     description: 'Budget web app',
     githubLink: 'https://github.com/leovoon/vue-budget-app/',
     liveLink: 'https://vuebudget.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/budget.jpg',
+      webp: 'img/webp/budget.webp',
+    },
   },
   {
     name: 'my own snippet blog',
     description: 'blog with wordpress public api',
     githubLink: 'https://github.com/leovoon/leo-blog',
     liveLink: 'https://leocs.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/snippet-blog.jpg',
+      webp: 'img/webp/snippet-blog.webp',
+    },
   },
   {
     name: 'vue only blog',
     description: 'markdown-based PWA with vitesse',
     githubLink: 'https://github.com/leovoon/explore-vue',
     liveLink: 'https://vnoted.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/vnoted.jpg',
+      webp: 'img/webp/vnoted.webp',
+    },
   },
   {
     name: 'Moviii',
     description: 'typical movie listing and searching site',
     githubLink: 'https://github.com/leovoon/vue-movie-app',
     liveLink: 'https://moviii.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/moviii.jpg',
+      webp: 'img/webp/moviii.webp',
+    },
   },
   {
     name: 'Cleanfolio',
     description: 'A clean portfolio website template',
     githubLink: 'https://github.com/leovoon/cleanfolio-vue',
     liveLink: 'https://cleanfolio.netlify.app/',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/cfolio.jpg',
+      webp: 'img/webp/cfolio.webp',
+    },
   },
   {
     name: 'Calorii',
@@ -82,6 +115,9 @@ export const projectList = [
     githubLink: 'https://github.com/leovoon/calorii',
     liveLink:
       'https://play.google.com/store/apps/details?id=com.leodev.calorii',
-    image: '#',
+    image: {
+      jpg: 'img/jpg/caloriii.jpg',
+      webp: 'img/webp/caloriii.webp',
+    },
   },
 ]
