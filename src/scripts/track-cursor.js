de7f490edae6a5f166425282a6f7002c70c1e1b9
Leo Voon <voonlihhaur@gmail.com>
2023-07-25 17:45:27 +0800

    fix: only run animation when isIntercepting

--- src/scripts/track-cursor.js
+++ src/scripts/track-cursor.js
@@ -1,6 +1,39 @@
-const updateCursor = ({ x, y }) => {
+let x = 0,
+  y = 0;
+let mouseX = 0,
+  mouseY = 0;
+
+const updateCursor = () => {
+  x += (mouseX - x) * 0.1;
+  y += (mouseY - y) * 0.1;
+
   document.documentElement.style.setProperty("--x", x);
   document.documentElement.style.setProperty("--y", y);
+
+  requestAnimationFrame(updateCursor);
 };
 
-document.body.addEventListener("pointermove", updateCursor);
+const handlePointerMove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+};
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // If #post-container is in view, remove the pointermove event listener.
+      document.body.addEventListener("pointermove", handlePointerMove);
+    } else {
+      // If #post-container is not in view, add the pointermove event listener.
+      document.body.removeEventListener("pointermove", handlePointerMove);
+    }
+  });
+});
+
+// Start observing #posts-container.
+let target = document.getElementById("posts-container");
+console.log("found", target);
+observer.observe(target);
+
+// Initialize cursor movement.
+updateCursor();
