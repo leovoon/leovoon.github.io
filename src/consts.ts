6c665d7ee5ddf2aa8a5d988a257c339c7227cfcb
Leo Voon <voonlihhaur@gmail.com>
2023-07-27 18:36:03 +0800

    update projects

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "lv.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Welcome to leo's website!";
 export const SITE_AUTHOR = "leovoon";
 export const SITE_GITHUB = "https://github.com/leovoon";
