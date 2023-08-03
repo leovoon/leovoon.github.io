241a70b8c44e7e41c8575c0ca252296d976ebea3
Leo Voon <voonlihhaur@gmail.com>
2023-08-03 19:36:53 +0800

    feat: add progress bar

---
+++ src/components/svelte/Article.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import FormattedDate from "./FormattedDate.svelte";
+  import { Motion, useViewportScroll, useTransform } from "svelte-motion";
+
+  export let pubDate: Date;
+  export let updatedDate: Date;
+  export let heroImage: string;
+  export let tags: string[];
+
+  const { scrollYProgress } = useViewportScroll();
+  const initial = useTransform(scrollYProgress, (x) => (x > 1 ? 1 : x + 0.04));
+</script>
+
+<article
+  class="relative prose prose-slate lg:prose-lg dark:prose-invert prose-hr:my-2"
+>
+  <Motion
+    let:motion
+    style={{
+      scaleY: initial,
+      originY: 0,
+    }}
+  >
+    <div
+      use:motion
+      class="w-1 h-[300px] bg-amber-500 dark:bg-amber-700 top-1/4 left-0 sm:left-4 xl:left-[20%] fixed"
+    />
+  </Motion>
+  <div
+    class="w-1 h-[300px] bg-stone-400/20 top-1/4 left-0 sm:left-4 xl:left-[20%] fixed scale-y-100"
+  />
+  {#if heroImage}
+    <img width={720} height={360} src={heroImage} alt={`${tags[0]} image`} />
+  {/if}
+  <FormattedDate date={pubDate} />
+  {#if updatedDate}
+    <div class="last-updated-on">
+      Last updated on <FormattedDate date={updatedDate} />
+    </div>
+  {/if}
+  {#if tags}
+    <div class="flex gap-x-2 gap-y-1.5 flex-wrap mt-2">
+      {#each tags as tag}
+        <div
+          class="list-none border text-sm border-amber-400 dark:border-amber-500 inline px-1.5 rounded"
+        >
+          {tag}
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  <slot />
+  <hr />
+</article>
---
+++ src/components/svelte/FormattedDate.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let date: Date;
+  export let classList: string = "";
+</script>
+
+<time class={classList} datetime={date.toISOString()}>
+  {date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })}
+</time>
