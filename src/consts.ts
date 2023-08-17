29eec1418d7f83643bcebd814a2a64363656f3db
Leo Voon <voonlihhaur@gmail.com>
2023-08-17 11:25:30 +0800

    meta: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "lv.";
-export const SITE_DESCRIPTION = "Welcome to leo's website!";
+export const SITE_DESCRIPTION = "Step into Leo's website, where I'm taking notes on web development topics through my blog posts.";
 export const SITE_AUTHOR = "leovoon";
 export const SITE_GITHUB = "https://github.com/leovoon";
