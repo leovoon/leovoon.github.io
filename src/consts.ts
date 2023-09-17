dd13a0676ff94cb60cee1fbf275227d3e7c44236
Leo Voon <voonlihhaur@gmail.com>
2023-09-17 12:18:04 +0800

    update: bun, add biome, move css file

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "lv.";
-export const SITE_DESCRIPTION = "Step into Leo's website, where I'm taking notes on web development topics through my blog posts.";
+export const SITE_DESCRIPTION =
+  "Step into Leo's website, where I'm taking notes on web development topics through my blog posts.";
 export const SITE_AUTHOR = "leovoon";
 export const SITE_GITHUB = "https://github.com/leovoon";
