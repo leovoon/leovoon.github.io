dfea825048218dfc812fb77db94abb84a14ce7ea
Leo Voon <voonlihhaur@gmail.com>
2024-01-25 17:07:50 +0800

    update links

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,7 @@ export const SITE_DESCRIPTION =
   "Step into Leo's website, where I'm taking notes on web development topics through my blog posts.";
 export const SITE_AUTHOR = "leovoon";
 export const SITE_GITHUB = "https://github.com/leovoon";
+export const SITE_MEDIUM = "https://medium.com/@leovoon";
+export const SITE_TWITTER = "https://twitter.com/voon_leo";
+export const SITE_LINKEDIN = "https://www.linkedin.com/in/leovoon/";
+export const SITE_EMAIL = "voonlihhaur@gmail.com";
